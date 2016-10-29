5b9ec0dbf094f7df6a9ad076d3f5d53d9accd7b9
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2016-10-29 09:11:24 +0900

    カレンダー作成用に新規ブランチを作成

--- sgp/app/helpers/application_helper.rb
+++ sgp/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # 条件が true の場合のみ、divタグを表示する
+  def div_tag_if(condition, content_or_options_with_block = nil, options = nil, escape = true, &block)
+    # condition が trueの場合だけ、content_tagメソッドでdivタグを作成する
+    # falseの場合は、何も作成しない
+    if condition
+      content_tag("div", content_or_options_with_block, options, escape, &block)
+    end
+  end
 end
