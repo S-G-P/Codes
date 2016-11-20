308a410541105f7deb9fbcde9cfe44de7bb688c2
Daisuke Awaji <daisuke.awaji@i.softbank.jp>
2016-11-20 11:19:25 +0000

    ScheduleUserテーブルを作成

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,7 @@
 class Schedule < ActiveRecord::Base
+  has_many :schedule_users
+  has_many :users, through: :schedule_users
+
+  # throughオプションによりschedule_users経由でusersにアクセスできるようになる
+  # 具体的には、schedule.usersでうユーザーにアクセスができる
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161029120323) do
+ActiveRecord::Schema.define(version: 20161120103826) do
+
+  create_table "schedule_users", force: :cascade do |t|
+    t.integer  "schedule_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "schedules", force: :cascade do |t|
     t.string   "title"
