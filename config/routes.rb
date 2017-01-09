03394664c3ca99f2e79bef4efcb0729726e714ea
Daisuke Awaji <daisuke.awaji@i.softbank.jp>
2017-01-09 05:39:50 +0000

    sign up, update useraccountの際にemail, password以外にNameを必須項目とする

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  
   get 'home/index'
   root 'home#index'
 
