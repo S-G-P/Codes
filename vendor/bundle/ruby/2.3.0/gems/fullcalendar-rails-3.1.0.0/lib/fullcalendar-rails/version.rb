51a5b45de648c548956fb338c630a5b99f67ad10
Daisuke Awaji <daisuke.awaji@i.softbank.jp>
2017-01-14 05:41:54 +0000

    gitignoreの対象ファイルを削除

--- vendor/bundle/ruby/2.3.0/gems/fullcalendar-rails-3.1.0.0/lib/fullcalendar-rails/version.rb
+++
@@ -1,5 +0,0 @@
-module Fullcalendar
-  module Rails
-    VERSION = "3.1.0.0"
-  end
-end
--- vendor/bundle/ruby/2.3.0/gems/remotipart-1.3.1/lib/remotipart/rails/version.rb
+++
@@ -1,6 +0,0 @@
-module Remotipart
-  module Rails
-    VERSION = "1.3.1"
-    IFRAMETRANSPORT_VERSION = "02.06.2013"
-  end
-end
